946fdb79d147a5c353816b74e40b5ad811d1b618
marcellbalog <marcell.balog@gmail.com>
2020-10-11 23:11:19 +0200

    Save to file

--- 5_ora_project/5_ora_project/Form1.cs
+++ 5_ora_project/5_ora_project/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,7 @@ namespace _5_ora_project
 		List<Tick> Ticks;
 
 		List<PortfolioItem> Portfolio = new List<PortfolioItem>();
-
+		List<decimal> Nyereségek = new List<decimal>();
 
 		public Form1()
 		{
@@ -57,11 +58,11 @@ namespace _5_ora_project
 
 		void CountVaR()
 		{
-			List<decimal> Nyereségek = new List<decimal>();
 			int intervalum = 30;
 			DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
 			DateTime záróDátum = new DateTime(2016, 12, 30);
 			TimeSpan z = záróDátum - kezdőDátum;
+
 			for (int i = 0; i < z.Days - intervalum; i++)
 			{
 				decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
@@ -75,6 +76,43 @@ namespace _5_ora_project
 									  select x)
 										.ToList();
 			MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+		}
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			SaveToFile();
+		}
+
+		void SaveToFile()
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+
+			sfd.DefaultExt = "csv";
+			sfd.AddExtension = true;
+
+			if (sfd.ShowDialog() != DialogResult.OK)
+				return;
+
+
+			using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+			{
+				sw.Write("Időszak; Nyereség");
+				for (int i = 0; i < Nyereségek.Count; i++)
+				{
+					// Egy ciklus iterációban egy sor tartalmát írjuk a fájlba
+					// A StreamWriter Write metódusa a WriteLine-al szemben nem nyit új sort
+					// Így darabokból építhetjük fel a csv fájl pontosvesszővel elválasztott sorait
+					sw.Write(i);
+					sw.Write(";");
+					sw.Write(Ticks[i].TradingDay);
+					sw.Write(";");
+					sw.Write(Nyereségek[i]);
+					sw.WriteLine(); // Ez a sor az alábbi módon is írható: sr.Write("\n");
+				}
+			}
+
+
 		}
 	}
 }
