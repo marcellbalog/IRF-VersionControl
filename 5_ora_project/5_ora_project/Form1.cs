c5ffa85f924c34106f2fb06ab823c3e8d49b8c8f
marcellbalog <marcell.balog@gmail.com>
2020-10-11 22:53:58 +0200

    Count VaR

--- 5_ora_project/5_ora_project/Form1.cs
+++ 5_ora_project/5_ora_project/Form1.cs
@@ -26,6 +26,8 @@ namespace _5_ora_project
 			dataGridView1.DataSource = Ticks;
 
 			CreatePortfolio();
+
+			CountVaR();
 		}
 
 		private void CreatePortfolio()
@@ -52,5 +54,27 @@ namespace _5_ora_project
 			return value;
 		}
 
+
+		void CountVaR()
+		{
+			List<decimal> Nyereségek = new List<decimal>();
+			int intervalum = 30;
+			DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+			DateTime záróDátum = new DateTime(2016, 12, 30);
+			TimeSpan z = záróDátum - kezdőDátum;
+			for (int i = 0; i < z.Days - intervalum; i++)
+			{
+				decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+						   - GetPortfolioValue(kezdőDátum.AddDays(i));
+				Nyereségek.Add(ny);
+				Console.WriteLine(i + " " + ny);
+			}
+
+			var nyereségekRendezve = (from x in Nyereségek
+									  orderby x
+									  select x)
+										.ToList();
+			MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+		}
 	}
 }
