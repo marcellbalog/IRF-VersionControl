9656757e7c96ba12a0b322a6d3f5d039d406eb61
marcellbalog <marcell.balog@gmail.com>
2020-10-11 22:52:24 +0200

    Calculate portfolio value

--- 5_ora_project/5_ora_project/Form1.cs
+++ 5_ora_project/5_ora_project/Form1.cs
@@ -36,5 +36,21 @@ namespace _5_ora_project
 
 			dataGridView2.DataSource = Portfolio;
 		}
+
+		private decimal GetPortfolioValue(DateTime date)
+		{
+			decimal value = 0;
+			foreach (var item in Portfolio)
+			{
+				var last = (from x in Ticks
+							where item.Index == x.Index.Trim()
+							   && date <= x.TradingDay
+							select x)
+							.First();
+				value += (decimal)last.Price * item.Volume;
+			}
+			return value;
+		}
+
 	}
 }
