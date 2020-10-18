1b990304ea99fe668792b8687bf65376de1fd054
marcellbalog <marcell.balog@gmail.com>
2020-10-18 19:41:06 +0200

    Chart with data

--- 6_ora/6_ora/Form1.cs
+++ 6_ora/6_ora/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace _6_ora
@@ -17,12 +18,15 @@ namespace _6_ora
 	{
 		BindingList<RateData> Rates = new BindingList<RateData>();
 
+		BindingList<string> Currencies = new BindingList<string>();
+
 		public Form1()
 		{
 			InitializeComponent();
 			//CallService();
 			dataGridView1.DataSource = Rates;
 			ProcessXML(CallService());
+			LoadChart();
 		}
 
 		string CallService()
@@ -31,9 +35,9 @@ namespace _6_ora
 
 			var request = new GetExchangeRatesRequestBody()
 			{
-				currencyNames = "EUR",
-				startDate = "2020-01-01",
-				endDate = "2020-06-30"
+				currencyNames = comboBox1.SelectedItem.ToString(),
+				startDate = dateTimePicker1.Value.ToString(),
+				endDate = dateTimePicker2.Value.ToString()
 			};
 
 			var response = mnbService.GetExchangeRates(request);
@@ -63,6 +67,47 @@ namespace _6_ora
 					rd.Value = value / unit;
 
 			}
+		}
+
+		void LoadChart()
+		{
+			chartRateData.DataSource = Rates;
+
+			var series = chartRateData.Series[0];
+			series.ChartType = SeriesChartType.Line;
+			series.XValueMember = "Date";
+			series.YValueMembers = "Value";
+			series.BorderWidth = 2;
+
+			var legend = chartRateData.Legends[0];
+			legend.Enabled = false;
+
+			var chartArea = chartRateData.ChartAreas[0];
+			chartArea.AxisX.MajorGrid.Enabled = false;
+			chartArea.AxisY.MajorGrid.Enabled = false;
+			chartArea.AxisY.IsStartedFromZero = false;
+
+		}
+
+		void RefreshData()
+		{
+			Rates.Clear();
+			LoadChart();
+		}
+
+		private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+		{
+			RefreshData();
+		}
+
+		private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+		{
+			RefreshData();
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			RefreshData();
 		}
 	}
 }
