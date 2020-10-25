d8e9ab1abd9a3432b9fe0390de163add64f5f617
marcellbalog <marcell.balog@gmail.com>
2020-10-25 22:04:50 +0100

    Classes created

---
+++ 7_ora/7_ora/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ora.Entities
+{
+	class BirthProbability
+	{
+		public int Age { get; set; }
+		public int ChildCount { get; set; }
+		public double P { get; set; }
+	}
+}
---
+++ 7_ora/7_ora/Entities/Gender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ora.Entities
+{
+	public enum Gender
+	{
+		Male = 1,
+		Female = 2
+	}
+
+
+
+}
